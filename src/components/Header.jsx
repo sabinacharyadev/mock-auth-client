7144cc01fc1cd9947260aac9b99265fb1ac06837
sabinbabu <sabinacharya444@gmail.com>
2025-03-28 16:41:13 +1100

    dropdown added to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserAction } from "../redux/userAction";
+import { uppercaseFirstLetter } from "../utility/stringHelper";
 const Header = () => {
   const { user } = useSelector((state) => state.users);
   const dispatch = useDispatch();
@@ -15,10 +16,13 @@ const Header = () => {
         <Navbar.Brand href="#home">Auth System</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="mx-2">Signed in as:{user.name}</Navbar.Text>
-          <Navbar.Text style={{ cursor: "pointer" }} onClick={handleOnClick}>
-            Logout
-          </Navbar.Text>
+          <NavDropdown
+            title={uppercaseFirstLetter(user.name)}
+            id="basic-nav-dropdown"
+          >
+            {/* <NavDropdown.Divider /> */}
+            <NavDropdown.Item onClick={handleOnClick}>Logout</NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
---
+++ src/utility/stringHelper.js
@@ -0,0 +1,3 @@
+export const uppercaseFirstLetter = (string) => {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+};
